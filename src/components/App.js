e38384b6325460ac2fe49c1b95e8305cc0034d5d
blake thomas <blaketaylorthomas@gmail.com>
2017-09-07 19:16:53 -0400

    add charts, move logic to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,61 @@
 import React, { Component } from 'react';
 import logo from '../images/reddit-icon.png';
 import './App.css';
-import Posts from '../containers/Posts.js';
-// import Dashboard from '../containers/Dashboard.js';
+import Posts from '../components/Posts.js';
+import Dashboard from '../components/Dashboard.js';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      redditResponse: null,
+      collapsed: true
+    };
+
+    this.toggleCollapsed = this.toggleCollapsed.bind(this);
+
+    this.fetchReddit();
+  }
+
+  toggleCollapsed() {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
+
+  fetchReddit() {
+    fetch('https://www.reddit.com/.json')
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          redditResponse: json
+        });
+      })
+      .catch(err => {
+        console.log('there was an error with fetchReddit');
+        console.log(err);
+      });
+  }
+
   render() {
+    const postProps = {
+      ...this.state,
+      handleClick: this.toggleCollapsed
+    };
+
+    const dashProps = {
+      ...this.state,
+    };
+
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to the reddit dashboard</h2>
         </div>
-        <Posts />
+        <Posts {...postProps} />
+        <Dashboard {...dashProps} />
       </div>
     );
   }
---
+++ src/components/Dashboard.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import { BarChart, Bar, XAxis, YAxis, Tooltip } from 'recharts';
+
+class Dashboard extends Component {
+  constructor() {
+    super();
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+
+    this.props.handleClick();
+  }
+
+  getData() {
+    const posts = this.props.redditResponse.data.children.map(post => post.data);
+    const now = new Date().getTime() / 1000;
+
+    if ( !posts.length ) {
+      return [];
+    }
+
+    return posts.map((post, index) => {
+      return {
+        rank: index + 1,
+        age: 8 + (now - post.created) / 3600,
+        ...post
+      }
+    })
+
+  }
+
+  renderChart(property, color) {
+    const { width, height } = getSize();
+
+    return (
+      <BarChart width={width/2} height={height/2} data={this.getData()}>
+        <XAxis dataKey="rank" />
+        <YAxis />
+        <Tooltip />
+        <Bar 
+          fill={color}
+          dataKey={property}
+        />
+      </BarChart>
+    );
+  }
+
+  render() {
+    const charts = [
+      {
+        title: 'Karma', 
+        key: 'score',
+        color: "#8811dd",
+      },
+      {
+        title: 'Comments', 
+        key: 'num_comments',
+        color: "#dd8811",
+      },
+      {
+        title: 'Age', 
+        key: 'age',
+        color: "#11dd88",
+      },
+      {
+        title: 'Gold', 
+        key: 'gilded',
+        color: "#dd1188",
+      },
+    ];
+
+    return this.props.redditResponse && (
+      <div className="chartWrapper">
+        {charts.map(chart => {
+          return (
+            <div className="chart" key={chart.key}>
+              <h3 className="chart-title">{chart.title}</h3>
+              {this.renderChart(chart.key, chart.color)}
+            </div>
+          )
+        })}
+      </div>
+    );
+  }
+}
+
+function getSize() {
+  return {
+    width: 1300,
+    height: 800
+  }
+}
+
+export default Dashboard;
