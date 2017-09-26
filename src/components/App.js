61a0ac48dc4fe5abcae073d71c422d6543b57175
blake thomas <blaketaylorthomas@gmail.com>
2017-09-26 16:30:02 -0400

    mobx 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,37 @@
 import React, { Component } from 'react';
+import { action, observable } from 'mobx';
+import { observer } from 'mobx-react';
 import logo from '../images/reddit-icon.png';
 import './App.css';
 import Posts from '../components/Posts.js';
 import Dashboard from '../components/Dashboard.js';
 
+@observer
 class App extends Component {
   constructor() {
     super();
 
-    this.state = {
+    this.myState = observable({
       redditResponse: null,
       collapsed: true
-    };
+    });
 
     this.toggleCollapsed = this.toggleCollapsed.bind(this);
 
     this.fetchReddit();
   }
 
+  @action
   toggleCollapsed() {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
+    this.myState.collapsed = !this.myState.collapsed;
   }
 
+  @action
   fetchReddit() {
     fetch('https://www.reddit.com/.json')
       .then(res => res.json())
       .then(json => {
-        this.setState({
-          redditResponse: json
-        });
+        this.myState.redditResponse = json;
       })
       .catch(err => {
         console.log('there was an error with fetchReddit');
@@ -40,12 +41,12 @@ class App extends Component {
 
   render() {
     const postProps = {
-      ...this.state,
+      ...this.myState,
       handleClick: this.toggleCollapsed
     };
 
     const dashProps = {
-      ...this.state,
+      ...this.myState,
     };
 
     return (
